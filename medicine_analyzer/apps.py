9128475173b1d70e91637d4c111a84e0b9357e00
siwon <siwongim033@gmail.com>
2025-09-30 22:05:47 +0900

    first commit

---
+++ medicine_analyzer/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class MedicineAnalyzerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'medicine_analyzer'
+    verbose_name = '약물 분석기'
